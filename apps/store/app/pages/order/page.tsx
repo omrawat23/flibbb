6ffa7e4b620170cf3d1d27b2dda1ed3facdee454
omrawat23 <omrawat23@gmail.com>
2024-07-12 22:12:59 +0530

    data

--- apps/store/app/pages/order/page.tsx
+++ apps/store/app/pages/order/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+'use client';
+
 import React, { useState, useEffect, DragEvent } from 'react';
 import { Product } from '../../types';
 import CheckoutPage from '../../../components/CheckoutPage';
@@ -242,3 +244,4 @@ const OrderForm: React.FC<Props> = ({ updateQuantity, removeFromCart }) => {
 };
 
 export default OrderForm;
+
