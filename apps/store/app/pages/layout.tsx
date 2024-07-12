324f74159080712fb5ce99ee42c6c32fe419b1e5
omrawat23 <omrawat23@gmail.com>
2024-07-13 02:06:54 +0530

    data

--- apps/store/app/pages/layout.tsx
+++ apps/store/app/pages/layout.tsx
@@ -1,11 +1,11 @@
 import MaxWidthWrapper from '../../components/MaxWidthWrapper'
 import { ReactNode } from 'react'
-
+import Navbar from '../../components/Navbar'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className='flex-1 flex flex-col'>
-     
+     <Navbar/>
       {children}
     </MaxWidthWrapper>
   )
